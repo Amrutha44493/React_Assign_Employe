01a1c051616645c315c56261d50de1b41d7d7cde
amrutha <amrutha@cenddle.com>
2025-03-27 23:38:56 +0530

    css updation

--- src/components/Employee.jsx
+++ src/components/Employee.jsx
@@ -3,7 +3,14 @@ import { Box, Button, TextField, Typography } from '@mui/material'
 const Employee = () => {
   return (
 <div className="form-container">   
-             <Box className="box" sx={{ width: 500, maxWidth: '100%' }}>
+             <Box className="box"  sx={{
+          width: 500,
+          maxWidth: '100%',
+          border: '1px solid #ccc', 
+          borderRadius: '8px', 
+          boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)', 
+          padding: '20px', 
+        }}>
                 <Typography variant="h5">Employee Form</Typography>
                 <TextField
                     fullWidth
