a536539513165a0baaade87343fe0d6cabb44e65
amrutha <amrutha@cenddle.com>
2025-03-28 10:28:02 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -9,13 +9,18 @@ function App() {
 
   return (
     <>
-<Navbar/>
-  <Routes>
-    <Route path='/' element={<Home/>}> </Route>
-    <Route path='/employee' element={<Employee/>}> </Route>
-  </Routes>   
+    <BrowserRouter basename="/React_Assign_Employe">
+      <Routes>
+        <Route index element={<Home />} />
+        
+        <Route path="home" element={<Home />} />
+        <Route path="employee" element={<Employee />} />
+      </Routes>
+    </BrowserRouter>  
    </>
   )
 }
 
-export default App
+
+
+export default App;
